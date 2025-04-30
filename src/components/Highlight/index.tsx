cb667d9452cb1ff8e2dde139ae38e301b60dd9fa
plenarc <200726885+plenarc@users.noreply.github.com>
2025-04-30 15:41:27 +0900

    .

--- src/components/Highlight/index.tsx
+++ src/components/Highlight/index.tsx
@@ -2,19 +2,26 @@
  * Module Text highlighting. Color designation is possible.
  * @module Highlight
  * @example
- * import TBD from '@site/src/components/Highlight';
+ * import { Highlight } from '@site/src/components/Highlight';
  * <Highlight color='#25C2A0'>Green</Highlight>
  * <Highlight color='#1877F2'>Blue</Highlight>
  * <TBD/>
  */
-import React from 'react';
+import type { ReactNode } from 'react';
+
+type HighlightProps = {
+  /** 強調したい文字列（子要素） */
+  children: React.ReactNode;
+  /** 背景色（16進数表記 #RRGGBB か #RGB） */
+  color: string;
+};
 
 /**
  * Highlight tag.
  * @param {{chidren: string, color: string}}
  * @returns {JSX.Element}
  */
-function Highlight({children, color}) {
+export function Highlight({children, color}: HighlightProps): ReactNode {
   return (
     <span
       style={{
@@ -30,24 +37,23 @@ function Highlight({children, color}) {
 
 /**
  * @see {@link https://www.w3.org/TR/AERT/#color-contract}
- * @param {string} - hex example. '11AAFF' or '1AF'
- * @return {string} '#FFF' or '#000'
+ * @param  hex - example. '11AAFF' or '1AF'
  */
-function blackOrWhite(hex) {
+function blackOrWhite(hex: string): '#FFF' | '#000' {
   const [r, g, b] = hex2rgb(hex);
   return ((((r * 299) + (g * 587) + (b * 114)) / 1000) < 120) ? '#FFF' : '#000';
 }
 
 /**
- * @param {string} - hexVal example. '11AAFF' or '1AF'
- * @return {number[]} [reg, green, blue]
+ * @param hexVal - example. '11AAFF' or '1AF'
  */
-function hex2rgb(hexVal) {
-  let hex = hexVal;
-  if (/^#/.test(hex)) hex = hex.slice(1);
+function hex2rgb(hexVal: string): [number, number, number] {
+let hex = hexVal.startsWith('#') ? hexVal.slice(1) : hexVal;
+
   if (![3, 6].includes(hex.length)) {
-    throw new Error(`Invalid hex string. digit are 3 or 6. hex=${hex}`);
+    throw new Error(`Invalid hex string: digit must be 3 or 6. hex=${hexVal}`);
   }
+
   if (hex.length === 3) hex = hex.replace(/(.)/g, '$1$1');
   if (!/^[0-9A-Fa-f]{6}$/.test(hex)) {
     throw new Error(`Invalid hex string. not hex number. hex=${hex}`);
@@ -57,7 +63,9 @@ function hex2rgb(hexVal) {
     throw new Error(`Invalid hex string. unknown. hex=${hex}`);
   }
 
-  return [parseInt(rgb[0], 16), parseInt(rgb[1], 16), parseInt(rgb[2], 16)];
+  return [
+    Number.parseInt(rgb[0], 16),
+    Number.parseInt(rgb[1], 16),
+    Number.parseInt(rgb[2], 16)
+  ];
 }
-
-export default Highlight
---
+++ src/components/TBD/index.tsx
@@ -0,0 +1,15 @@
+/**
+ * Module TBD Label.
+ * @module TBD
+ * @example
+ * import { TBD } from '@site/src/components/TBD';
+ * <TBD/>
+ */
+import type { ReactNode } from 'react';
+import { Highlight } from '../Highlight/index';
+
+export function TBD(): ReactNode {
+  return (
+    <Highlight color='#1877F2'>TBD</Highlight>
+  )
+}
